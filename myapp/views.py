43ab5274a1fbe5d420cde0790602ffaae30b6807
gaur-abhishek <gaur2195@gmail.com>
2017-07-19 17:19:02 +0530

    Added time functionality

--- myapp/views.py
+++ myapp/views.py
@@ -3,6 +3,7 @@ from forms import SignUpForm
 from models import User
 from django.http import HttpResponse
 from django.contrib.auth.hashers import make_password
+from datetime import datetime, timedelta
 
 # Create your views here.
 
@@ -12,5 +13,6 @@ def signup_view(request):
         print 'Sign up form submitted'
     elif request.method == 'GET':
         form = SignUpForm()
-
-    return render(request, 'index.html', {'form' : form})
+    five_hours_thirty_mins_from_now = datetime.now() + timedelta(hours=5.5)
+    today = five_hours_thirty_mins_from_now
+    return render(request, 'index.html',{'today': today}, {'form' : form})
